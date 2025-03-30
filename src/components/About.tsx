24951c453cd16b321a14a0237b1278f1414b8e35
Kendra Mulligan <kmulligan589@gmail.com>
2025-03-30 16:27:47 -0600

    fixed small css issue

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,7 +14,7 @@ export default function About() {
                 <p className="my-8 not-prose"><span className="text-lg text-cyan-blue  font-bold">After</span> completing my six month bootcamp, I started working as a frontend developer for a web solution business that specialized in assisting home improvement contractors. Here, I quickly worked myself up to managing a small team of 2 other developers. Together, we completed upwards of 5-8 sites a day utilizing JavaScript, HTML, CSS, and other frameworks.</p>
                 <p><span className="text-lg text-cyan-blue  font-bold">I was</span> hired by EverCommerce Solutions in 2021 as a mid-level developer where I have earned 4 awards for my teamwork and strategy within different projects. I was promoted to Senior level developer in 2023. Some of the technologies and languages I work with include React, JavaScript, GraphQL, Headless WordPress, email templates, marketing attribution, and lead generation.</p>
             </div>
-            <div className="grid lg:grid-cols-4 gap-4 mt-12 md:grid-cols-2 grid-cols-1 space-y-4 ">
+            <div className="grid lg:grid-cols-4 gap-4 mt-12 md:grid-cols-2 grid-cols-1 lg:space-y-0 space-y-4 ">
                 <Card icon={TabletSmartphone} label="Mobile-First Responsive Design" />
                 <Card icon={Chrome} label="Cross Browser Testing " />
                 <Card icon={Users} label="Cross Functional Teams" />
